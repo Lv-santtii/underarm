2897245303586c48a28db1f630e828bdcf74dc7a
Lv-santtii <santinoguidali@gmail.com>
2024-05-05 01:54:42 -0300

    S

--- app.js
+++ app.js
@@ -10,7 +10,6 @@ const Usuario = require('./app/models/usuario');
 const authMiddleware = require('./app/middlewares/authMiddleware');
 const multer = require('multer');
 const { z } = require('zod');
-const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
@@ -20,7 +19,7 @@ const port = process.env.PORT || 3020;
 app.use(authMiddleware);
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(cors());
+
 // Configuracion de las sesiones //
 app.use(session({
   secret: 'clave123',
